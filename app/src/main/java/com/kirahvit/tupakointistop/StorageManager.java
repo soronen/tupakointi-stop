3fde78d8c1c48fb1098dba911141bd5d7ff1656c
Rasmus <rasmuny@metropolia.fi>
2021-09-22 15:22:36 +0300

    StorageManager -luokan lisäys, luokassa on metodeja, joita voidaan käyttää arvojen tallentamiseen ja lukemiseen

---
+++ app/src/main/java/com/kirahvit/tupakointistop/StorageManager.java
@@ -0,0 +1,48 @@
+package com.kirahvit.tupakointistop;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+/**
+ * Luokka sisältää SharedPreferences tallentamista ja lukemista helpottavia metodeja
+ * @author Rasmus Nygård
+ */
+
+public class StorageManager {
+
+    private Context context;
+    private SharedPreferences sharedPreferences;
+
+    public StorageManager(Context context){
+        this.context = context;
+        sharedPreferences = context.getSharedPreferences("com.kirahvit.app",Context.MODE_PRIVATE);
+    }
+
+    /**
+     * Palauttaa tallennetun arvon, joka vastaa annettua tunnistetta
+     * @param name tunnistenimi, jolla haetaan tallennettua arvoa
+     * @return int loadedValue löydetty arvo, oletusarvo 0
+     */
+    public int loadValue(String name){
+
+        int loadedValue = sharedPreferences.getInt(name, 0);
+        Log.d("logger", name + " value: " + loadedValue + " loaded.");
+
+        return loadedValue;
+    }
+
+    /**
+     * Tallentaa arvon annetulle tunnisteelle
+     * @param name tunnistenimi
+     * @param value arvo, joka tallennetaan tunnisteelle
+     */
+    public void saveValue(String name, int value){
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putInt(name, value);
+        editor.commit();
+        Log.d("logger", name + " value: " + value + " saved.");
+    }
+}
