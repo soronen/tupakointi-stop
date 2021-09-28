844124205caaa6d380dfded8cf236d44c23decc9
soronen <eetu.soronen@hotmail.com>
2021-09-28 16:54:57 +0300

    hieman toimivia lisäyksiä settings luokkaan

--- app/src/main/java/com/kirahvit/tupakointistop/Settings.java
+++ app/src/main/java/com/kirahvit/tupakointistop/Settings.java
@@ -16,95 +16,97 @@ import android.widget.TextView;
 public class Settings extends AppCompatActivity {
     private static final String TAG = "Settings";
 
-    private int mMaara;
-    private int hHinta;
-    private int pPaivat;
-
+    private int maara;
+    private int hinta;
+    private int paivat;
 
 
     // sharedpref tuonti, väliaikainen..
-
     private SharedPreferences sharedPref;
     private SharedPreferences.Editor editor;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_settings);
 
         // shared preferenssien tuonti, väliaikainen.. + niiden liittäminen muuttujiin joita käytetään tässä aktiviteetissa.
-        sharedPref = getSharedPreferences("maara" , Activity.MODE_PRIVATE);
-        hHinta = sharedPref.getInt("maara", 0);
+        sharedPref = getSharedPreferences("hinta" , Activity.MODE_PRIVATE);
+        hinta = sharedPref.getInt("hinta", 0);
 
-        sharedPref = getSharedPreferences("hinta" ,Activity.MODE_PRIVATE);
-        mMaara = sharedPref.getInt("hinta", 0);
+        sharedPref = getSharedPreferences("maara" ,Activity.MODE_PRIVATE);
+        maara = sharedPref.getInt("maara", 0);
 
         sharedPref = getSharedPreferences("paivat" ,Activity.MODE_PRIVATE);
-        pPaivat = sharedPref.getInt("paivat", 0);
+        paivat = sharedPref.getInt("paivat", 0);
 
 
         // tupakoiden määrän ja hinnan textView
         TextView tupakoidenMaara = (TextView) findViewById(R.id.editTextMaara);
-        tupakoidenMaara.setText(String.valueOf(mMaara));
+        tupakoidenMaara.setText(String.valueOf(maara));
 
         TextView tupakoidenHinta = (TextView) findViewById(R.id.editTextHinta);
-        tupakoidenHinta.setText(String.valueOf(hHinta));
+        tupakoidenHinta.setText(String.valueOf(hinta));
+
+
+        // kun tupakoille syötetään hinta tai määrä ja painetaan entteriä, nämä tallentavat ne aktiviteetin muuttujiin.
+        EditText editHinta = (EditText) findViewById(R.id.editTextHinta);
+        editHinta.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean boo = false;
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    //Perform your Actions here.
+                    hinta = Integer.parseInt(tupakoidenHinta.getText().toString());
+                    Log.d(TAG, "Enter (hinta) pressed");
+                }
+                return boo;
+            }
+        });
+
+        EditText editMaara = (EditText) findViewById(R.id.editTextMaara);
+        editMaara.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean boo = false;
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    //Perform your Actions here.
+                    maara = Integer.parseInt(tupakoidenMaara.getText().toString());
+                    Log.d(TAG, "Enter (maara) pressed");
+                }
+                return boo;
+            }
+        });
 
 
         Log.d(TAG, "onCreate() called");
 
     }
 
-
     public void resetPressed (View v) {
-        pPaivat = 0;
-
+        paivat = 0;
     }
-    // nämä eivät toimi
-
-    public void updateMaara (View view) {
-        EditText editTextMaara = (EditText) findViewById(R.id.editTextMaara);
-        int mMaara = Integer.parseInt(editTextMaara.getText().toString());
-    }
-    public void updateHinta (View view) {
-        EditText editTextHinta = (EditText) findViewById(R.id.editTextHinta);
-        int hHinta = Integer.parseInt(editTextHinta.getText().toString());
-
-    }
-
-    final TextView.OnEditorActionListener exampleListener = new TextView.OnEditorActionListener() {
-        @Override
-        public boolean onEditorAction(TextView editTextHinta, int i, KeyEvent KEYCODE_ENTER) {
-            boolean handled = false;
-            if (9001 == EditorInfo.IME_ACTION_DONE) {
-                editTextHinta = (EditText) findViewById(R.id.editTextHinta);
-                hHinta = Integer.parseInt(editTextHinta.getText().toString());
-                handled = true;
-            }
-            return handled;
-        }
-    };
-
 
     // tämän olisi tarkoitus tallentaa arvot SharedPreferensseihin.
     @Override
     public void onPause() {
         super.onPause();
 
-        SharedPreferences hits = getSharedPreferences("maara" , Activity.MODE_PRIVATE);
-        editor = hits.edit();
-        editor.putInt("maara", mMaara);
+        sharedPref = getSharedPreferences("maara" , Activity.MODE_PRIVATE);
+        editor = sharedPref.edit();
+        editor.putInt("maara", maara);
         editor.commit();
 
         sharedPref = getSharedPreferences("hinta",Activity.MODE_PRIVATE);
         editor = sharedPref.edit();
-        editor.putInt("hinta", hHinta);
+        editor.putInt("hinta", hinta);
         editor.commit();
 
         sharedPref = getSharedPreferences("paivat",Activity.MODE_PRIVATE);
         editor = sharedPref.edit();
-        editor.putInt("paivat", pPaivat);
+        editor.putInt("paivat", paivat);
         editor.commit();
 
         Log.d(TAG, "onPause() called");
